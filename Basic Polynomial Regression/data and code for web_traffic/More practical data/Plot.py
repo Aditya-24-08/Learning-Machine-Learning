e020cb6caa4ef914e9aec518491d06b775d03e81
Aditya Rastogi <r.aditya0824@gmail.com>
2018-01-07 18:26:12 +0530

    added folder Basic Polynomial Regression

---
+++ Basic Polynomial Regression/data and code for web_traffic/More practical data/Plot.py
@@ -0,0 +1,19 @@
+def Plot(n,x,y):
+    import scipy as sp
+    import matplotlib.pyplot as plt
+    #assuming data is per hour
+    data = sp.genfromtxt("web_traffic.tsv",delimiter="\t")
+    x = x[~sp.isnan(y)]
+    y=y[~sp.isnan(y)]
+    plt.scatter(x,y,marker='+',color='g')
+    plt.title("Web traffic")
+    plt.xlabel("time")
+    plt.ylabel("hits/hr")
+    plt.xticks([w*7*24 for w in range(10)],['week %i' %w for w in range(10)])
+    fpn = sp.polyfit(x,y,n)
+    fn = sp.poly1d(fpn)
+    fx = sp.linspace(0,x[-1],1000) #generate x values for plotting
+    plt.plot(fx,fn(fx),color='k',linewidth = 3)
+    plt.legend(["d=%i"%fn.order],loc = "upper center")
+    plt.grid()
+    plt.show()
---
+++ Basic Polynomial Regression/data and code for web_traffic/Not so good data/readme.txt
@@ -0,0 +1,3 @@
+Just regression, that's all I have done here in this beginning
+folder. Which curve best fits the data without overfitting it
+is the core issue upon which I have pondered in this module.
